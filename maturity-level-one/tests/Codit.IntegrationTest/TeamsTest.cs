1000afc7b3d2601881d0092790366e5a94bd62b3
MassimoC <massimo.crippa@gmail.com>
2019-01-09 19:22:08 +0100

    teams test added

---
+++ maturity-level-one/tests/Codit.IntegrationTest/TeamsTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Hosting;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using Codit.LevelOne.Models;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Headers;
+
+namespace Codit.IntegrationTest
+{
+    public class TeamsTest
+    {
+        private readonly HttpClient _httpClient;
+        public TeamsTest()
+        {
+            if (_httpClient != null) return;
+            var srv = new TestServer(new WebHostBuilder()
+                .UseEnvironment("Development")
+                .UseStartup<Codit.LevelOne.Startup>());
+
+            _httpClient = srv.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task GetTeams_Ok_TestAsync(string httpMethod)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(httpMethod), "/world-cup/v1/teams");
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+    }
+}
