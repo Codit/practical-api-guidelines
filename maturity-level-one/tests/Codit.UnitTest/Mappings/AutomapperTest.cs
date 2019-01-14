04d6762c94bdeaeec8ec40200ddc214e07baa61e
MassimoC <massimo.crippa@gmail.com>
2019-01-14 12:54:39 +0100

    act

--- maturity-level-one/tests/Codit.UnitTest/Mappings/AutomapperTest.cs
+++ maturity-level-one/tests/Codit.UnitTest/Mappings/AutomapperTest.cs
@@ -2,9 +2,6 @@
 using Codit.LevelOne;
 using Codit.LevelOne.Entities;
 using Codit.LevelOne.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Xunit;
 using FluentAssertions;
 
@@ -12,11 +9,15 @@ namespace Codit.UnitTest.Mappings
 {
     public class AutomapperTest
     {
+        public AutomapperTest()
+        {
+            AutoMapperConfig.Initialize();
+        }
 
         [Fact]
         public void Map_PlayerToDto_Test()
         {
-            AutoMapperConfig.Initialize();
+            //Arrange
             var objInstance = new Player
             {
                 FirstName = "Mario",
@@ -24,23 +25,19 @@ namespace Codit.UnitTest.Mappings
                 IsTopPlayer = true,
                 TeamId = 1
             };
+            var playerDto = new PlayerDto();
 
-            var dtoInstance = new PlayerDto();
-            dtoInstance = Mapper.Map<PlayerDto>(objInstance);
+            //Act
+            playerDto = Mapper.Map<PlayerDto>(objInstance);
 
-            dtoInstance.Should().NotBeNull();
-            dtoInstance.Id.Should().Be(objInstance.Id);
-            dtoInstance.FirstName.Should().Be(objInstance.FirstName);
-            dtoInstance.Description.Should().Be(objInstance.Description);
-            dtoInstance.IsTopPlayer.Should().Be(objInstance.IsTopPlayer);
-            dtoInstance.TeamId.Should().Be(objInstance.TeamId);
+            //Assert
+            playerDto.Should().NotBeNull();
+            playerDto.Id.Should().Be(objInstance.Id);
+            playerDto.FirstName.Should().Be(objInstance.FirstName);
+            playerDto.Description.Should().Be(objInstance.Description);
+            playerDto.IsTopPlayer.Should().Be(objInstance.IsTopPlayer);
+            playerDto.TeamId.Should().Be(objInstance.TeamId);
 
-            dtoInstance.Should().BeEquivalentTo(objInstance, options => options
-                .Including(o => o.Id)
-                .Including(o => o.FirstName)
-                .Including(o => o.Description)
-                .Including(o => o.IsTopPlayer)
-                .Including(o => o.TeamId));
         }
     }
 }
