009152d327ef8c9753ca601c6135756b51c3f18d
MassimoC <massimo.crippa@gmail.com>
2019-01-14 14:28:23 +0100

    xml docs added

--- maturity-level-one/tests/Codit.IntegrationTest/TestExtensions.cs
+++ maturity-level-one/tests/Codit.IntegrationTest/TestExtensions.cs
@@ -10,6 +10,14 @@ namespace Codit.IntegrationTest
 {
     public static class TestExtensions
     {
+        /// <summary>
+        /// Build the HttpRequestMessage
+        /// </summary>
+        /// <param name="data">Object instance to be serialized</param>
+        /// <param name="method">HTTP method</param>
+        /// <param name="requestUri">Request URI</param>
+        /// <param name="contentType">Content Type of the request</param>
+        /// <returns></returns>
         public static HttpRequestMessage GetJsonRequest(object data, string method, string requestUri, string contentType = "application/json")
         {
             var serializedData = JsonConvert.SerializeObject(data);
@@ -22,16 +30,27 @@ namespace Codit.IntegrationTest
             return request;
         }
 
-        public static void ShouldBeNotNull(this HttpResponseMessage response, string mimeType="application/json")
+        /// <summary>
+        /// Validate the Length and the ContentType headers
+        /// </summary>
+        /// <param name="response">instance of HttpResponseMessage</param>
+        /// <param name="mediaType">Expected media type</param>
+        public static void ShouldBeNotNull(this HttpResponseMessage response, string mediaType = "application/json")
         {
-            response.Content.Should().NotBeNull();
-            response.Content.Headers.ContentType.MediaType.Should().Be(mimeType);
+            response.Content.Headers.ContentLength.Should().NotBeNull();
+            response.Content.Headers.ContentType.MediaType.Should().Be(mediaType);
         }
 
-        public static void ShouldContainContent(this HttpResponseMessage response, string mimeType, string expectedJson)
+        /// <summary>
+        /// Validate the content of a HttpResponseMessage
+        /// </summary>
+        /// <param name="response">instance of HttpResponseMessage</param>
+        /// <param name="mediaType">Expected media type</param>
+        /// <param name="expectedJson">Expected json content</param>
+        public static void ShouldContainContent(this HttpResponseMessage response, string mediaType, string expectedJson)
         {
-            response.Content.Should().NotBeNull();
-            response.Content.Headers.ContentType.MediaType.Should().Be(mimeType);
+            response.Content.Headers.ContentLength.Should().NotBeNull();
+            response.Content.Headers.ContentType.MediaType.Should().Be(mediaType);
 
             string serializeObject = JsonConvert.SerializeObject(response.Content.ReadAsAsync<dynamic>().Result);
             serializeObject.Should().Be(expectedJson);
