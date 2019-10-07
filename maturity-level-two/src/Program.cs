2ff7d92d4767d983e0886c077aefbc43eac2f499
Filip Van Raemdonck <filip.vanraemdonck@codit.eu>
2019-10-07 16:51:12 +0200

    Move migration to startup

--- maturity-level-two/src/Program.cs
+++ maturity-level-two/src/Program.cs
@@ -1,13 +1,38 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using Codit.LevelTwo.Entities;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Codit.LevelTwo
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<CoditoContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
